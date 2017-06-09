750c8f597d507fd0a3f7788ec98154c0ab7b23b0
Angello Pozo <Angello.pozo@gmail.com>
2017-06-08 23:57:35 -0700

    initial commit

---
+++ README.md
@@ -0,0 +1,19 @@
+Purpose:
+---
+Provide a demo applications that does signing and validation. The user will create a signature, and then validate it against a Solidity  smart contract.
+
+Requirements:
+----
+1. Truffle 3.2.1 or greater globally installed
+2. A running Ethereum node
+3. Node 4 or greater
+4. NPM 3 or greater
+
+Setup:
+----
+1. Start privae Ethereum node (`geth --datadir ... --networkid ... --mine --minerthreads 1 --rpc ...`)
+2. Clone repo
+3. cd into repo
+4. npm install
+5. npm run deploy
+6. npm run validate
---
+++ index.js
@@ -0,0 +1,59 @@
+const contract = require('truffle-contract')
+const Web3 = require('web3')
+const provider = new Web3.providers.HttpProvider('http://localhost:8545')
+const web3 = new Web3(provider)
+
+const VerifierArtifact = require('./build/contracts/Verifier')
+const Verifier = contract(VerifierArtifact)
+Verifier.setProvider(provider)
+
+function toHex(str) {
+	var hex = '';
+	for(var i=0;i<str.length;i++) {
+		hex += ''+str.charCodeAt(i).toString(16);
+	}
+	return hex;
+}
+
+const addr = web3.eth.accounts[0]
+const msg = 'school bus'
+const hex_msg = '0x' + toHex(msg)
+let signature = web3.eth.sign(addr, hex_msg)
+
+console.log(`address -----> ${addr}`)
+console.log(`msg ---------> ${msg}`)
+console.log(`hex(msg) ----> ${hex_msg}`)
+console.log(`sig ---------> ${signature}`)
+
+signature = signature.substr(2);
+const r = '0x' + signature.slice(0, 64)
+const s = '0x' + signature.slice(64, 128)
+const v = '0x' + signature.slice(128, 130)
+const v_decimal = web3.toDecimal(v)
+
+console.log(`r -----------> ${r}`)
+console.log(`s -----------> ${s}`)
+console.log(`v -----------> ${v}`)
+console.log(`vd ----------> ${v_decimal}`)
+
+Verifier
+  .deployed()
+  .then(instance => {
+    const fixed_msg = `\x19Ethereum Signed Message:\n${msg.length}${msg}`
+    const fixed_msg_sha = web3.sha3(fixed_msg)
+    return instance.recoverAddr.call(
+      fixed_msg_sha,
+      v_decimal,
+      r,
+      s
+    )
+  })
+  .then(data => {
+    console.log('-----data------')
+    console.log(`input addr ==> ${addr}`)
+    console.log(`output addr => ${data}`)
+  })
+  .catch(e => {
+    console.log('i got an error')
+    console.log(e)
+  })
